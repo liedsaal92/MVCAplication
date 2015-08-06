b148384ffd05206a54d60283bdbb4c930d8876ef
Lincoln <liedsaal92@hotmail.es>
2015-08-06 10:43:20 -0500

    mi primera github

---
+++ MvcApplication/Controllers/StoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Browse(string gente)
+        {
+            string mensaje = HttpUtility.HtmlEncode("store.browse, gente="+gente);
+            return mensaje;
+        }
+        public string Details(int id)
+        {
+            string mensaje = "store.browse, ID=" + id;            
+            return "hello from store.details"+mensaje;
+        }
+
+    }
+}
---
+++ MvcApplication/paginas/index.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/paginas/masterPage.Master" Inherits="System.Web.Mvc.ViewPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="script" runat="server">
+</asp:Content>
